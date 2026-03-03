4172dcb7ea75761d3467abbed94549bd4b6b909e
kanteti0501 <kiranmai.k@aol.com>
2026-03-02 23:32:54 -0700

    Adding files for review

---
+++ Str_meth.py
@@ -0,0 +1,13 @@
+# Program to explain some string functions
+
+str=input("Enter a sentence")
+
+print(f"The encoded version of sgtring is: ",str.encode())
+
+print(f"The string is alphanumeric : ",str.isalnum())
+
+sampletup = {"Apple","banana","mango","orange"}
+print("#".join(sampletup))
+
+# print(f"The partition of string is: ",str.partition())
+
---
+++ String_python.py
@@ -0,0 +1,23 @@
+# To display string operations in Python
+
+str='a'
+print(str)
+str='Hi..Welcoime to Python'
+print(str)
+str='''We hold these truths to be self evident that all men are created equal
+that they are assigned by certain unalienable rights and among these are
+life, liberty and pursuity of happiness. That to secure these rights
+governments are institued among men to derive the just powers from 
+the consent of the government'''
+print(f"The declaration of independence",str)
+
+str1='We the People'
+str2="of United States of America"
+print(str1+str2)
+print(str1*3)
+
+print(len(str))
+
+print(str.find("liberty"))
+print(str2.upper())
+print(str1.lower())
